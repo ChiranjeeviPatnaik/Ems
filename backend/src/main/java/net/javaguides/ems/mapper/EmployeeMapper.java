d495afb48faad06186f4f66c6cc3ea7e6987076e
ChiranjeeviPatnaik <chiranjeevipatnaik00@gmail.com>
2025-08-07 17:47:22 +0530

    backend changes

---
+++ backend/src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,30 @@
+package net.javaguides.ems.mapper;
+
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmailId(),
+                employee.getRole(),
+                employee.getPhoneNumber()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmailId(),
+                employeeDto.getRole(),
+                employeeDto.getPhoneNumber()
+        );
+    }
+
+}
